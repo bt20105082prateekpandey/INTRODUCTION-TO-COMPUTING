9077cc585b70808f23228d497c0f34c561c68de3
bt20105082prateekpandey <97735483+bt20105082prateekpandey@users.noreply.github.com>
2022-01-14 21:17:32 +0530

    Add files via upload

---
+++ Question1.py
@@ -0,0 +1,13 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+number_1=input("Please give number 1: ")
+number_2=input("Please give number 2: ")
+number_3=input("Please give number 3: ")
+
+number_1=int(number_1)
+number_2=int(number_2)
+number_3=int(number_3)
+#average
+avg=(number_1+number_2+number_3)/3
+print(avg)
