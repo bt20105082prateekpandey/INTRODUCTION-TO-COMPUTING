04a753b8568a7a09169b6ced6ba7ac5fa9a489ed
bt20105082prateekpandey <97735483+bt20105082prateekpandey@users.noreply.github.com>
2022-02-14 23:46:45 +0530

    Add files via upload

---
+++ Assignment3_output.py
@@ -0,0 +1,73 @@
+QUESTION 1
+ENTER ANY STRING: parteek
+{'P': 1, 'A': 1, 'R': 1, 'T': 1, 'E': 1, 'E': 1, 'K': 1}
+
+
+QUESTION 2
+ENTER THE DAY: 11
+ENTER THE MONTH OF THE YEAR: 5
+ENTER THE YEAR: 2001
+12 / 5 / 2001
+
+
+QUESTION 3
+Enter elements of a list separated by space 5 8 11
+list:  5 8 11
+[(5, 25), (8, 64), (11, 121)]
+
+
+QUESTION 4
+Enter Grade Point: 8
+Your Grade is 'B+' and Very Good performance
+
+
+QUESTION 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+QUESTION 6
+Enter SID: 20105082
+Enter Name: parteek
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 20105083
+Enter Name: alpha
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 20105084
+Enter Name: Bita
+Do you want to enter another student entry(Y or N):N
+<a> {20105082: 'parteek', 20105044: 'alpha', 20105045: 'Bita'}
+<b> {20105084: 'Bita', 20105044: 'alpha', 20105043: 'parteek'}
+<c> {20105082: 'parteek', 20105083: 'alpha', 20105084: 'Bita'}
+Search Name with SID: 20105082
+<d> parteek
+
+
+QUESTION 7
+ENTER THE NUMBER OF TERMS IN THE SERIES: 12
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+AVERAGE OF RESULTANT FIBONACCI SERIES IS 19.333333333333332
+
+
+QUESTION 8
+<a> {1, 3, 5, 6, 8}
+<b> {3, 6, 8, 9, 13, 17}
+<c> {1, 2, 4, 5}
+<d> {6, 7, 8, 9, 10}
+<e> {10, 7}
