f7d0273e2858418b539bd4b3b85478fc1de0c5b5
bt20105082prateekpandey <97735483+bt20105082prateekpandey@users.noreply.github.com>
2022-03-01 21:53:38 +0530

    Add files via upload

---
+++ Assignment-4 output.py
@@ -0,0 +1,74 @@
+# ANSWER 1
+# Move disk 1 from rod A to rod C
+# Move disk 2 from rod A to rod B
+# Move disk 1 from rod C to rod B
+# Move disk 3 from rod A to rod C
+# Move disk 1 from rod B to rod A
+# Move disk 2 from rod B to rod C
+# Move disk 1 from rod A to rod C
+
+
+# ANSWER 2
+# Enter the size of pascals triangle 8
+# USING FOR LOOP
+#          1
+#         1 1
+#        1 2 1
+#       1 3 3 1
+#      1 4 6 4 1
+#     1 5 10 10 5 1
+#    1 6 15 20 15 6 1
+#   1 7 21 35 35 21 7 1
+
+
+
+# USING WHILE LOOP
+#          1
+#         1 1
+#        1 2 1
+#       1 3 3 1
+#      1 4 6 4 1
+#     1 5 10 10 5 1
+#    1 6 15 20 15 6 1
+#   1 7 21 35 35 21 7 1
+
+
+
+# USING RECURSSIONS
+#               1
+#             1   1
+#           1   2   1
+#         1   3   3   1
+#       1   4   6   4   1
+#     1   5   10   10   5   1
+#   1   6   15   20   15   6   1
+# 1   7   21   35   35   21   7   1
+
+
+# ANSWER 3
+# Enter 2 numbers: 4 7
+# a. Checking if the quotient and remainder is a callable value or not.
+# False
+# False
+# <b> The quotient is zero
+# <c> Filtered out numbers that are greater than 4 are : [8, 9, 5, 9, 6, 10]
+# <d> Set: {5, 6, 8, 9, 10}
+# <e> Immutable set: frozenset({5, 6, 8, 9, 10})
+# <f> Hash value of the max value from the above set: 10
+
+
+# ANSWER 4
+# Object created
+# The name of the student is Prateek Pandey and SID is 20105082.
+# Object destroyed
+
+
+# ANSWER 5
+# <a> The updated salary of Mehak is 70000
+# <b>Record of Viren deleted
+
+# ANSWER 6
+# Enter the word: korf
+# Enter a new meaningful word using the exact same letters to test your friendship: fork
+# Does the word makes sense?(y/Y or n/N )y
+# The friend pass the friendship test
